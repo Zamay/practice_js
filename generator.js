23765b94bf72245b58a9c73d3913a52e22d29b33
Zamay <mityazamay@gmail.com>
2017-05-10 18:31:40 +0300

    add 4, 6, 8

--- generator.js
+++ generator.js
@@ -41,33 +41,83 @@
 // console.log(map(square, [1,2,3,4])); // [1, 4, 9, 16]
 //
 // № 4
-
-
+//
+// var fmap=(a, gen)=>(x,...args)=>a(gen(x,...args))
+//
+// function fmap(a, gen) {
+//     return function (x, ...args) {
+//         return a(gen(x, ...args));
+//     }
+// }
+//
+// function add(a, b) {
+//     return a + b;
+// }
+// function square(x) {
+//     return x * x;
+// }
+//
+// var squareAdd = fmap(square, add);
+// console.log(squareAdd(2, 3))
+//
 // № 5
 // const partial  = (fn , ...args) => (...restArgs) => fn( ...args.concat(restArgs));
-
+//
 // №6 ---------
-
-        function pluck (objects, fieldName){
-            var arr = [];
-            objects.forEach(
-                function (obj) {
-                    arr[1]
-                    return arr;
-                });
-            console.log(arr)
-        }
-        pluck(characters, 'name')
-
+//
+// /----------------------------------------------------/
+//
+// function partialAny(func, ...args) {
+//     let args = Array.from(arguments);
+//     args.splice(0, 1);
+//     return function () {
+//         let arrInside = Array.from(arguments);
+//
+//         for (let i = 0; i < args.length; i++) {
+//             if (args[i] === undefined) {
+//                 args[i] = arrInside.shift();
+//             }
+//         }
+//         args = args.concat(arrInside);
+//         return func.apply(this, args);
+//     }
+// }
+//
+// function test(...args) {
+//     return args;
+//
+// }
+//
+// var test1_3 = partialAny(test, 1, undefined, 3, 88, undefined);
+// console.log(test1_3(57, 15, 33, 24, 34));
+//
+// var test1_5 = partialAny(test, 66, undefined, 3, undefined, undefined);
+// console.log(test1_5(10, 55, 33));
+//
 // № 7
 // function bind(fn, context) {
 //     return function() { // (*)
 //         return fn.apply(context, arguments);
 //     };
 // }
-
+//
 // № 8
-
+//
+// let user = [
+//     { 'name': 'barney', 'age': 36 },
+//     { 'name': 'fred', 'age': 40 }
+// ];
+//
+// function pluck(objects, fieldName) {
+//     let arr = [];
+//     for (let obj of objects){
+//         arr.push(obj[fieldName]);
+//         console.log(arr);
+//     }
+//     return arr;
+// }
+// pluck(user, 'name');
+//
 // № 9
 // var input = [1, 2, 3, 4, 5, 6];
 // function isEven(x) { return x % 2 == 0; } // проверяет на четность
@@ -82,7 +132,7 @@
 // }
 //
 // console.log(filter(input, isEven)); // [2, 4, 6]
-
+//
 // № 10
 //
 // function count (obj) {
