0fd7cec701d030940b84ca0f2940f9d481f7d9ef
Zamay <mityazamay@gmail.com>
2017-05-11 10:26:33 +0300

    Задача 11

---
+++ number_11.js
@@ -0,0 +1,24 @@
+/**
+ * Created by zamaj on 11.05.2017.
+ */
+
+var jsonFile = new XMLHttpRequest();
+jsonFile.open('GET', 'https://gist.githubusercontent.com/fayvlad/f89533efa1376468fcfd61f7ec3f544e/raw/e587cbbb2229c2e81f2d77a4be119f80dd52f024/task%2520%252311%2520js', false);
+jsonFile.send();
+var data = JSON.parse(jsonFile.responseText);
+
+    // data.sort(function (a, b) {  // так или ...
+let sortArr = data.sort(function (a, b) {
+    var popA = a.population, popB = b.population;
+    return (popA > popB) ? -1 : 1;
+});
+
+// debugger
+let i = 0;
+for (let arr of sortArr){
+    if (i < 10){
+        console.log(arr);
+        i++;
+    }
+}
+
